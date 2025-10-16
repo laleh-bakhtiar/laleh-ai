3ec51e71bc39b80877497e8e93515b2b5f12d3f1
misinc <karim@misinc.net>
2025-10-16 13:26:07 -0600

    Hide navbar on laleh ai

--- laleh-ai/pages/_app.js
+++ laleh-ai/pages/_app.js
@@ -1,11 +1,16 @@
 // pages/_app.js
 import '../styles/globals.css';
 import Navigation from '../components/Navigation';
+import { useRouter } from 'next/router';
 
 export default function App({ Component, pageProps }) {
+	const router = useRouter();
+	const HIDE_NAV_PATHS = ['/chat', '/laleh-ai'];
+	const showNavigation = !HIDE_NAV_PATHS.includes(router.pathname);
+
 	return (
 		<>
-			<Navigation />
+			{showNavigation && <Navigation />}
 			<Component {...pageProps} />
 		</>
 	);
